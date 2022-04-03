348bc31f49244a63919272ed6478d52ed509926d
Laura <lauramjohnson@me.com>
2022-04-02 23:49:37 -0500

    thumbnails

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Laura Johnson on 3/30/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
+    
+    let pages: [Page] = pagesData
+    @State private var pageIndex: Int = 1
     
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -19,12 +23,16 @@ struct ContentView: View {
         }
      }
     
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
+    }
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color.clear
                 // MARK:  - Page Image
-                Image("magazine-front-cover")
+                Image(currentPage())
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
@@ -35,6 +43,9 @@ struct ContentView: View {
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 // MARK:  - 1. Tap Gesture
+                    .onTapGesture(perform: {
+                        isDrawerOpen = false
+                    })
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
@@ -58,6 +69,28 @@ struct ContentView: View {
                                     withAnimation(.spring()) {
                                         resetImageState()
                                     }
+                                }
+                            }
+                    )
+                // MARK:  - Magnification
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    } else {
+                                        imageScale = 1
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
                                 }
                             }
                     )
@@ -119,6 +152,49 @@ struct ContentView: View {
                 }
                 .padding(.bottom, 30)
                 , alignment: .bottom
+            )
+            // MARK:  - drawer
+            .overlay(
+                HStack(spacing: 12) {
+                    // MARK:  - handle
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture(perform: {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    // MARK:  - Thumbnails
+                    
+                    ForEach(pages) { item in
+                        Image(item.thumbnailName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .opacity(isDrawerOpen ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isDrawerOpen)
+                            .onTapGesture(perform: {
+                                isAnimating = true
+                                pageIndex = item.id
+                            })
+                    }
+                    Spacer()
+                    
+                }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment: .topTrailing
             )
         }
         .navigationViewStyle(.stack)
